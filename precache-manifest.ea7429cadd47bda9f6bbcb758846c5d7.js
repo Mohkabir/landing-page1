be595b67acfdc39d2294a3d46d127dd054c2d833
mohammed kabir <kbshowkb@gmail.com>
2021-01-29 08:31:37 -1200

    Updates

---
+++ precache-manifest.ea7429cadd47bda9f6bbcb758846c5d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e7a793f0ea8478a18ec0ad06068068",
+    "url": "/landing-page1/index.html"
+  },
+  {
+    "revision": "5edc8e8629a183d2552c",
+    "url": "/landing-page1/static/css/main.747d783e.chunk.css"
+  },
+  {
+    "revision": "7e7460900e0aa70b257e",
+    "url": "/landing-page1/static/js/2.a9abd63f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/landing-page1/static/js/2.a9abd63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5edc8e8629a183d2552c",
+    "url": "/landing-page1/static/js/main.0a3bd3fb.chunk.js"
+  },
+  {
+    "revision": "bc433e9d359e3a14e652",
+    "url": "/landing-page1/static/js/runtime-main.e76a0fad.js"
+  },
+  {
+    "revision": "dc31c4eebb541925f441afdffe0909ef",
+    "url": "/landing-page1/static/media/01.dc31c4ee.jpg"
+  },
+  {
+    "revision": "a3c25e65a7324e9697b79bbadab1ca58",
+    "url": "/landing-page1/static/media/02.a3c25e65.jpg"
+  },
+  {
+    "revision": "8b0c6608147d118196e739374de2d533",
+    "url": "/landing-page1/static/media/03.8b0c6608.jpg"
+  }
+]);
